e41bb930778a2e78b58d082c890bddd03dd4c631
송진섭 <sjs9238@gmail.com>
2021-11-22 15:14:02 +0900

    추천 알고리즘 구현 좋아요 에러 수정

--- movies/views.py
+++ movies/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import get_list_or_404, get_object_or_404
 from .models import Movie, Review
-from django.views.decorators.http import require_POST
 from .serializers import MovieSerializer, ReviewSerializer
 
 @api_view(['GET'])
@@ -16,9 +15,39 @@ def index(request):
 @api_view(['GET'])
 def recommend_list(request):
     if request.method == 'GET':
-        movies = Movie.objects.all().order_by('?')[:10]
-        serializer = MovieSerializer(movies, many=True)
-        return Response(serializer.data)
+        user = request.user
+        like_movies = user.like_movies.all()
+        context = {}
+        for movie in like_movies:
+            for genre in movie.genres.all():
+                if genre.id not in context:
+                    context[genre.id] = 1
+                else:
+                    context[genre.id] += 1
+        context_sorted = sorted(context.items(), reverse=True, key=lambda item: item[1]) #[[28,4],[878,2],[12,1],[10752,1]]
+        if len(context_sorted) > 4:
+            movie1 = Movie.objects.all().filter(genres__in=[context_sorted[0][0]]).order_by('?')[:4]
+            movie2 = Movie.objects.all().filter(genres__in=[context_sorted[1][0]]).order_by('?')[:3]
+            movie3 = Movie.objects.all().filter(genres__in=[context_sorted[2][0]]).order_by('?')[:2]
+            movie4 = Movie.objects.all().filter(genres__in=[context_sorted[3][0]]).order_by('?')[:1]
+            movies = movie1 | movie2 | movie3 | movie4
+            serializer = MovieSerializer(movies, many=True)
+            return Response(serializer.data)    
+        elif len(context_sorted) > 2:
+            movie1 = Movie.objects.all().filter(genres__in=[context_sorted[0][0]]).order_by('?')[:6]
+            movie2 = Movie.objects.all().filter(genres__in=[context_sorted[1][0]]).order_by('?')[:4]
+            movies = movie1 | movie2
+            serializer = MovieSerializer(movies, many=True)
+            return Response(serializer.data)    
+        elif len(context_sorted) == 1:
+            movie1 = Movie.objects.all().filter(genres__in=[context_sorted[0][0]]).order_by('?')[:10]
+            serializer = MovieSerializer(movie1, many=True)
+            return Response(serializer.data)    
+        else:
+            movie1 = Movie.objects.all().order_by('?')[:10]
+            serializer = MovieSerializer(movie1, many=True)
+            return Response(serializer.data)    
+        
 
 @api_view(['GET'])
 def popularity_list(request):
@@ -71,15 +100,21 @@ def review_get_create(request, movie_pk):
             serializer.save(user=request.user, movie=movie)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['POST'])
+@api_view(['GET','POST'])
 def like(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
-    if movie.like_users.filter(pk=request.user.pk).exists():
-        movie.like_users.remove(request.user)
-        like = False
-    else:
-        movie.like_users.add(request.user)
-        like = True
+    if request.method == 'GET':
+        if movie.like_users.filter(pk=request.user.pk).exists():
+            like = True
+        else:
+            like = False
+    if request.method == 'POST':
+        if movie.like_users.filter(pk=request.user.pk).exists():
+            movie.like_users.remove(request.user)
+            like = False
+        else:
+            movie.like_users.add(request.user)
+            like = True
     context = {
         'like': like,
         'count': movie.like_users.count()
